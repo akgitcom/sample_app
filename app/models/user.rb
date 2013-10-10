6f965cc8b256602fc800b6fd19419ca7aab06946
mingming <aksoft@163.com>
2013-10-10 16:53:11 +0800

    第六章完结

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	
+	has_secure_password
 	before_save { self.email = email.downcase }
 	# before_save :downcase_email
 
@@ -8,13 +8,13 @@ class User < ActiveRecord::Base
 	# end
 	validates :name,  presence: true, length: { maximum: 50 }
 	
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true,
 				format: { with: VALID_EMAIL_REGEX },
 				uniqueness: { case_sensitive: false }
 	# validates(:name, presence: true)
 	# 使用的回调函数是 before_save，在用户存入数据库之前强行把 Email 地址转换成全小写字母形式
 	
-	has_secure_password
+	
 	validates :password, length: { minimum: 6 }
 end
