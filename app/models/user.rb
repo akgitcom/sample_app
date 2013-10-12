68fc2380a2e166d266514f606d67710c30d711bd
mingming <aksoft@163.com>
2013-10-12 13:32:26 +0800

    finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	before_save { self.email = email.downcase }
+	before_create :create_remember_token
 	# before_save :downcase_email
 
 	# def downcase_email
@@ -12,9 +13,21 @@ class User < ActiveRecord::Base
 	validates :email, presence: true,
 				format: { with: VALID_EMAIL_REGEX },
 				uniqueness: { case_sensitive: false }
-	# validates(:name, presence: true)
+	validates(:name, presence: true, uniqueness: true)
 	# 使用的回调函数是 before_save，在用户存入数据库之前强行把 Email 地址转换成全小写字母形式
-	
-	
 	validates :password, length: { minimum: 6 }
+
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
+
+	private
+
+		def create_remember_token
+			self.remember_token = User.encrypt(User.new_remember_token)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,18 @@ SampleApp::Application.routes.draw do
   # get "static_pages/about"
   # get "static_pages/contact"
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   match '/static_pages/home', :to =>'static_pages#home', via:'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
-  match '/signup',  to: 'users#new',            via: 'get'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
