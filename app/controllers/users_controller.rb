6a6ad99c5d54a0f0ac194813e715b6cc561ade25
mingming <aksoft@163.com>
2013-10-12 12:12:02 +0800

    add ssl in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
 	def show
 		@user = User.find(params[:id])
 	end
@@ -10,16 +9,6 @@ class UsersController < ApplicationController
 
 	def create
 		@user = User.new(user_params)    # Not the final implementation!
-=======
-	def new
-		@user = User.new
-	end
-	def show
-		@user = User.find(params[:id])
-	end
-	def create
-		@user = User.new(user_params)
->>>>>>> sign-up
 		if @user.save
 			flash[:success] = "Welcome to the Sample App!"
 			redirect_to @user
@@ -29,13 +18,7 @@ class UsersController < ApplicationController
 	end
 
 	private
-<<<<<<< HEAD
 	def user_params
 		params.require(:user).permit(:name, :email, :password,:password_confirmation)
-=======
-
-	def user_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation)
->>>>>>> sign-up
 	end
 end
